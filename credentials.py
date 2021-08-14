1cbda0ff1978e715bb5749a9d4bd1b093a0cbafb
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-08-14 22:21:39 +0530

    commit

---
+++ credentials.py
@@ -0,0 +1,3 @@
+from decouple import config
+
+TOKEN = config("TOKEN")
--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import telegram.ext as telegram_bot
+import credentials
 
+
+def start(update, context):
+    update.message.reply_text(
+        r"""
+    Made by JAK
+
+    /help -> See Full Commands List
+"""
+    )
+
+
+def help_me(update, context):
+    update.message.reply_text(
+        r"""
+    
+"""
+    )
+
+
+updater = telegram_bot.Updater(credentials.TOKEN, use_context=True)
+disp = updater.dispatcher
+
+disp.add_handler(telegram_bot.CommandHandler("start", start))
+disp.add_handler(telegram_bot.CommandHandler("help", help_me))
+
+updater.start_polling()
+updater.idle()
