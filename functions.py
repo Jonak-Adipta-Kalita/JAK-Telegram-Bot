cac39cf360bd1314efccd37075e9ecdb6b103e2b
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-08-15 10:07:55 +0530

    added some more commands

--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
-import webbrowser
+import webbrowser, datetime
 
 
 def start(update, context):
     update.message.reply_text(
         r"""
-    Made by JAK
+    Made by JAK aka Jonak Adipta Kalita
 
     /help -> See Full Commands List
 """
@@ -16,10 +16,12 @@ def help_me(update, context):
         r"""
     Full Commands List:
 
-    /start   -> Start the Bot
-    /help    -> See full List of Cammands
+    /start -> Start the Bot
+    /help -> See full List of Cammands
     /website -> Open JAK's Website
-    /api     -> Open JAK's API's Website
+    /api -> Open JAK's API's Website
+    /discord_bot -> Open JAK's Discord Bot Invite Link
+    /time -> See the Current Time
 """
     )
 
@@ -32,3 +34,16 @@ def website(update, context):
 def api(update, context):
     update.message.reply_text("Opening JAK's API's Website!!")
     webbrowser.open("https://jak-api-dot-com.herokuapp.com/")
+
+
+def discord_bot(update, context):
+    update.message.reply_text("Opening JAK's Discord Bot Invite Link!!")
+    webbrowser.open(
+        "https://discord.com/oauth2/authorize?client_id=756402881913028689&scope=bot"
+    )
+
+
+def time(update, context):
+    now = datetime.datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    update.message.reply_text(f"The Current Time is: {current_time}")
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ disp.add_handler(telegram_bot.CommandHandler("start", functions.start))
 disp.add_handler(telegram_bot.CommandHandler("help", functions.help_me))
 disp.add_handler(telegram_bot.CommandHandler("website", functions.website))
 disp.add_handler(telegram_bot.CommandHandler("api", functions.api))
+disp.add_handler(telegram_bot.CommandHandler("discord_bot", functions.discord_bot))
+disp.add_handler(telegram_bot.CommandHandler("time", functions.time))
 
 updater.start_polling()
 updater.idle()
