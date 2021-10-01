70a129af99f3077ed357b734fb9d76ce1a4a1209
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-10-01 11:02:25 +0530

    introduced src directory

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import telegram.ext as telegram_bot
-import credentials, functions
+import src.functions as funcs
+import credentials
 
 updater = telegram_bot.Updater(credentials.TOKEN, use_context=True)
 disp = updater.dispatcher
 
-disp.add_handler(telegram_bot.CommandHandler("start", functions.start))
-disp.add_handler(telegram_bot.CommandHandler("help", functions.help_me))
-disp.add_handler(telegram_bot.CommandHandler("time", functions.time))
+disp.add_handler(telegram_bot.CommandHandler("start", funcs.start))
+disp.add_handler(telegram_bot.CommandHandler("help", funcs.help_me))
+disp.add_handler(telegram_bot.CommandHandler("time", funcs.time))
 
 updater.start_polling()
 updater.idle()
