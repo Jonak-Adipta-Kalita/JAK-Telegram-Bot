13f5a799cc53a288c02d000bf9f20c1271dd52b4
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-10-06 18:47:30 +0530

    Webbrowser doesn't works in Heroku!!

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ disp = updater.dispatcher
 disp.add_handler(telegram_bot.CommandHandler("start", funcs.start))
 disp.add_handler(telegram_bot.CommandHandler("help", funcs.help_me))
 disp.add_handler(telegram_bot.CommandHandler("time", funcs.time))
-disp.add_handler(telegram_bot.CommandHandler("repo", funcs.repo))
-disp.add_handler(telegram_bot.CommandHandler("hacktoberfest", funcs.hacktoberfest))
 
 updater.start_polling()
 updater.idle()
--- src/functions.py
+++ src/functions.py
@@ -30,20 +30,3 @@ def time(update, context):
     now = datetime.datetime.now()
     current_time = now.strftime("%H:%M:%S")
     update.message.reply_text(f"The Current Time is: {current_time}")
-
-
-def hacktoberfest(update, context):
-    webbrowser.open("https://hacktoberfest.digitalocean.com/")
-    update.message.reply_text(
-        r"""
-    Hacktoberfest encourages participation in the open source community, which grows bigger every year. Complete the 2021 challenge and earn a limited edition T-Shirt.
-And also this bot is included in Hacktoberfest Repositories!!
-
-/repo -> Go to this Bot's Repository
-"""
-    )
-
-
-def repo(update, context):
-    webbrowser.open("https://github.com/Jonak-Adipta-Kalita/JAK-Telegram-Bot")
-    update.message.reply_text("Opening Bot's repository.")
