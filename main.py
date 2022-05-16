d9df795df308f977a4386698e265f08dba3d3c53
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-05-16 13:11:54 +0530

    refactored the code

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import credentials
 print("Starting...")
 
 updater = telegram_bot.Updater(credentials.TOKEN, use_context=True)
-disp = updater.dispatcher
 
-disp.add_handler(telegram_bot.CommandHandler("start", funcs.start))
-disp.add_handler(telegram_bot.CommandHandler("help", funcs.help_me))
-disp.add_handler(telegram_bot.CommandHandler("time", funcs.time))
-disp.add_handler(telegram_bot.CommandHandler("hacktoberfest", funcs.hacktoberfest))
+updater.dispatcher.add_handler(telegram_bot.CommandHandler("start", funcs.start))
+updater.dispatcher.add_handler(telegram_bot.CommandHandler("help", funcs.help_me))
+updater.dispatcher.add_handler(telegram_bot.CommandHandler("time", funcs.time))
+updater.dispatcher.add_handler(
+    telegram_bot.CommandHandler("hacktoberfest", funcs.hacktoberfest)
+)
 
 updater.dispatcher.add_error_handler(error)
 updater.start_polling()
--- src/functions.py
+++ src/functions.py
@@ -1,31 +1,20 @@
+import datetime, telegram
 import src.keyboard_buttons as keyboard_buttons
-import datetime
 
 
-def start(update, context):
+def start(update: telegram.Update, context):
     update.message.reply_text(
-        r"""
-    Made by JAK aka Jonak Adipta Kalita
-
-    /help -> See Full Commands List
-"""
+        "Made by JAK aka Jonak Adipta Kalita\n/help -> See Full Commands List"
     )
 
 
-def help_me(update, context):
+def help_me(update: telegram.Update, context):
     update.message.reply_text(
-        r"""
-    Full Commands List:
-
-    /start -> Start the Bot
-    /help -> See full List of Commands
-    /time -> See the Current Time
-    /hacktoberfest -> Check out about Hacktoberfest
-"""
+        "Full Commands List:\n\n/start -> Start the Bot\n/help -> See full List of Commands\n/time -> See the Current Time\n/hacktoberfest -> Check out about Hacktoberfest"
     )
 
 
-def time(update, context):
+def time(update: telegram.Update, context):
     now = datetime.datetime.now()
     current_time = now.strftime("%H:%M:%S")
     update.message.reply_text(f"The Current Time is: {current_time}")
@@ -33,9 +22,6 @@ def time(update, context):
 
 def hacktoberfest(update, context):
     update.message.reply_text(
-        r"""
-Hacktoberfest encourages participation in the open source community, which grows bigger every year. Complete the 2021 challenge and earn a limited edition T-shirt.
-And also this bot is included in Hacktoberfest eligibal Repositories. For more information check the urls below.
-        """,
+        "Hacktoberfest encourages participation in the open source community, which grows bigger every year. Complete the 2021 challenge and earn a limited edition T-shirt.\nAnd also this bot is included in Hacktoberfest eligibal Repositories. For more information check the urls below.",
         reply_markup=keyboard_buttons.hacktoberfest(),
     )
